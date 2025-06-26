f5b49654edfca3d03b8a4f483e6feaddae4725d2
marchbom <153171193+marchbom@users.noreply.github.com>
2025-06-26 09:05:39 +0900

    Title: 물의 상태, Time: 206ms, Memory: 15MB, Status: Wrong Answer - Codetree

--- 250626/물의 상태/state-of-water.js
+++ 250626/물의 상태/state-of-water.js
@@ -1,12 +1,11 @@
 const fs = require('fs');
 let input = fs.readFileSync(0).toString().trim().split(' ');
-
 let tem = Number(input[0]);
 
 if(tem < 0) {
     console.log('ice')
 } else if(tem > 100) {
     console.log('vapor');
-} else if(tem > 0 && tem < 100) {
+} else {
     console.log('water')
 }
