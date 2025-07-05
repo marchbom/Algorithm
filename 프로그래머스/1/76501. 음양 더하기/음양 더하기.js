3b29ccf15bd1565619573a750a8d9ebfe227e260
marchbom <153171193+marchbom@users.noreply.github.com>
2025-07-05 22:49:58 +0900

    [level 1] Title: 음양 더하기, Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.js
@@ -0,0 +1,7 @@
+function solution(absolutes, signs) {
+    let answer = 0
+    for(let i = 0; i < absolutes.length; i++) {
+        answer += absolutes[i] * (signs[i] ? 1 : -1)
+    }
+    return answer;
+}
