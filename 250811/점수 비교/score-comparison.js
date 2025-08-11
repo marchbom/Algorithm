5cdd231a86fb3369ac937e334156a58b4a5b603d
marchbom <153171193+marchbom@users.noreply.github.com>
2025-08-11 18:37:08 +0900

    Title: 점수 비교, Time: 178ms, Memory: 15MB, Status: Wrong Answer - Codetree

---
+++ 250811/점수 비교/score-comparison.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split("\n");
+
+let [aMath, aEng] = input[0].split(' ').map(Number);
+let [bMath, bEng] = input[1].split(' ').map(Number);
+
+if(aMath >= bMath && aEng >= bEng) {
+    return console.log('1')
+} else {
+    return console.log("0")
+}
