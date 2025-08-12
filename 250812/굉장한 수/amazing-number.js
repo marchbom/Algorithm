7df9e6580196cef2b5960ad970f942bc19b1fbfd
marchbom <153171193+marchbom@users.noreply.github.com>
2025-08-12 10:28:23 +0900

    Title: 굉장한 수, Time: 169ms, Memory: 15MB, Status: Passed - Codetree

--- 250812/굉장한 수/amazing-number.js
+++ 250812/굉장한 수/amazing-number.js
@@ -2,11 +2,11 @@ const fs = require('fs');
 const input = fs.readFileSync(0).toString().trim();
 
 
-let N = Number(input[0])
+let N = Number(input)
 
 if(N % 2 !== 0 && N % 3 === 0 ) {
     console.log("true")
-} else if(N % 2 === 0 && N % 5=== 0 ) {
+} else if(N % 2 === 0 && N % 5 === 0) {
     console.log("true")
 } else {
     console.log("false")
