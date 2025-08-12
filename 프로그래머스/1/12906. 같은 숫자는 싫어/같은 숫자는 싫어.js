f3ffbdbdca4b0d0249a31937d948e2b8fd440e41
marchbom <153171193+marchbom@users.noreply.github.com>
2025-08-12 11:02:13 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 37.27 ms, Memory: 83 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -1,9 +1,9 @@
-function solution(arr) {
+function solution(arr)
+{
     var answer = [];
-    for(let i = 0; i < arr.length; i++) {
-        if(arr[i] !== arr[i - 1]) {
-           answer.push(arr[i])
-        }
-    }
+
+    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    console.log('Hello Javascript')
+    
     return answer;
 }
