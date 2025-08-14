f38fdbec12a8b6839577d8a726cffac5d5543733
marchbom <153171193+marchbom@users.noreply.github.com>
2025-08-14 16:36:47 +0900

    [level 1] Title: 예산, Time: 0.32 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12982. 예산/예산.js
@@ -0,0 +1,11 @@
+function solution(d, budget) {
+    d.sort((a, b) => a-b)
+    let sum = 0;
+    let result = 0
+    for(let i =0; i < d.length; i++) {
+        if(sum + d[i] > budget) return result;
+        sum += d[i];
+        result++;
+    }
+    return result;
+}
