17ebdd173665fa450b4f34b3798c0d0f871419a9
marchbom <153171193+marchbom@users.noreply.github.com>
2025-08-15 14:12:44 +0900

    Title: 더 긴 문자열, Time: 191ms, Memory: 15MB, Status: Passed - Codetree

--- 250815/더 긴 문자열/longer-string.js
+++ 250815/더 긴 문자열/longer-string.js
@@ -1,13 +1,13 @@
 const fs = require('fs');
-const input = fs.readFileSync(0).toString().trim().split(/\s+/);
+const input = fs.readFileSync(0).toString().trim().split(" ");
 
 let a = input[0]
 let b = input[1]
 
 if(a.length > b.length) {
-    console.log(`Coding ${a.length}`)
+    console.log(`${a} ${a.length}`)
 } else if(a.length < b.length) {
-    console.log(`Coding ${b.length}`)
+    console.log(`${b} ${b.length}`)
 } else {
     console.log("same")
 }
