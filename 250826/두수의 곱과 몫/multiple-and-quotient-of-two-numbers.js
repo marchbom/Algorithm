997cf0bb369b1d5f1df4c03bc13c28f3c52f2e05
marchbom <153171193+marchbom@users.noreply.github.com>
2025-08-26 11:36:51 +0900

    Title: 두수의 곱과 몫, Time: 173ms, Memory: 15MB, Status: Passed - Codetree

---
+++ 250826/두수의 곱과 몫/multiple-and-quotient-of-two-numbers.js
@@ -0,0 +1,9 @@
+const fs = require('fs')
+const input = fs.readFileSync(0).toString().trim().split(' ').map(Number)
+
+
+let a = input[0]
+let b = input[1]
+
+console.log(`${a} * ${b} = ${a*b}`)
+console.log(`${a} / ${b} = ${parseInt(a/b)}`)
