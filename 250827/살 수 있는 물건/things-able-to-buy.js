6d1a8b09fcc7ae7d9541f51f81ff771db77983c8
marchbom <153171193+marchbom@users.noreply.github.com>
2025-08-27 00:31:00 +0900

    Title: 살 수 있는 물건, Time: 179ms, Memory: 15MB, Status: Passed - Codetree

---
+++ 250827/살 수 있는 물건/things-able-to-buy.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().trim().split('\n').map(Number);
+
+let n = input[0];
+
+if(n >= 3000) {
+    console.log("book")
+} else if(n >= 1000) {
+    console.log("mask")
+} else {
+    console.log("no")
+}
