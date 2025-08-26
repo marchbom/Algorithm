d38fe615b51c29f53d151a07eaddf24e9b7c865e
marchbom <153171193+marchbom@users.noreply.github.com>
2025-08-27 00:39:22 +0900

    Title: 등급 매기기, Time: 165ms, Memory: 15MB, Status: Passed - Codetree

--- 250827/등급 매기기/ranking.js
+++ 250827/등급 매기기/ranking.js
@@ -4,3 +4,7 @@ const input = fs.readFileSync(0).toString().trim().split('\n').map(Number);
 let score = input[0];
 
 if(score >= 90) console.log("A")
+else if(score >= 80) console.log("B")
+else if(score >= 70) console.log("C")
+else if(score >= 60) console.log("D")
+else console.log("F")
