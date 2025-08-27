618c08f03e6fb7b0261c46a2e6ada45da6535cd8
marchbom <153171193+marchbom@users.noreply.github.com>
2025-08-27 11:52:24 +0900

    Title: 4가지 관계연산자, Time: 187ms, Memory: 15MB, Status: Passed - Codetree

---
+++ 250827/4가지 관계연산자/four-relational-operators.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().trim().split(" ").map(Number);
+
+let a = input[0]
+let b = input[1]
+
+console.log(a >= b ? 1 : 0)
+console.log(a > b ? 1 : 0)
+console.log(b >= a ? 1 : 0)
+console.log(b > a ? 1 : 0)
