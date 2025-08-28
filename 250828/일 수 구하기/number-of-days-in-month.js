f8df281b83c509ce8d82e7a65d22aa4db4d64e58
marchbom <153171193+marchbom@users.noreply.github.com>
2025-08-28 13:26:06 +0900

    Title: 일 수 구하기, Time: 184ms, Memory: 15MB, Status: Passed - Codetree

---
+++ 250828/일 수 구하기/number-of-days-in-month.js
@@ -0,0 +1,33 @@
+const fs = require('fs')
+const input = fs.readFileSync(0).toString().trim().split('\n').map(Number)
+
+let n = input[0]
+
+
+
+if (n === 1) {
+  console.log(31);
+} else if (n === 2) {
+  console.log(28); 
+} else if (n === 3) {
+  console.log(31);
+} else if (n === 4) {
+  console.log(30);
+} else if (n === 5) {
+  console.log(31);
+} else if (n === 6) {
+  console.log(30);
+} else if (n === 7) {
+  console.log(31);
+} else if (n === 8) {
+  console.log(31);
+} else if (n === 9) {
+  console.log(30);
+} else if (n === 10) {
+  console.log(31);
+} else if (n === 11) {
+  console.log(30);
+} else if (n === 12) {
+  console.log(31);
+} 
+
