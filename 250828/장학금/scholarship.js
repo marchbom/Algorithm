0c63a6a0ff13b3b418b80883ba7f31cd008bb565
marchbom <153171193+marchbom@users.noreply.github.com>
2025-08-28 12:05:16 +0900

    Title: 장학금, Time: 185ms, Memory: 15MB, Status: Passed - Codetree

--- 250828/장학금/scholarship.js
+++ 250828/장학금/scholarship.js
@@ -4,4 +4,10 @@ const input = fs.readFileSync(0).toString().trim().split(" ").map(Number);
 let m = input[0]
 let l = input[1]
 
-console.log(m >= 90 ? (l >= 95 ? 100000 : 50000) : 0) 
+if(m >= 90 && l >= 95) {
+    console.log(100000)
+} else if (m >= 90 && l >=90) {
+    console.log(50000 )
+} else {
+    console.log(0)
+}
