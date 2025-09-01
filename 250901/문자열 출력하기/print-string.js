f14c4294263b72966ec75895319b642f87181e63
marchbom <153171193+marchbom@users.noreply.github.com>
2025-09-01 10:59:43 +0900

    Title: 문자열 출력하기, Time: 171ms, Memory: 15MB, Status: Passed - Codetree

---
+++ 250901/문자열 출력하기/print-string.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().trim().split("\n").map(Number)
+
+let n = input
+
+for(let i = 1; i <= n; i++) {
+    console.log("LeebrosCode")
+}
