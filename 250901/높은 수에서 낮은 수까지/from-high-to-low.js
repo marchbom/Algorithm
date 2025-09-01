c089907b46b44c6e47d74eb6aef89ad02dfb6a6e
marchbom <153171193+marchbom@users.noreply.github.com>
2025-09-01 12:05:51 +0900

    Title: 높은 수에서 낮은 수까지, Time: 191ms, Memory: 15MB, Status: Passed - Codetree

---
+++ 250901/높은 수에서 낮은 수까지/from-high-to-low.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().trim().split(" ").map(Number);
+
+input.sort((a, b) => a - b)
+let [a, b] = input;
+let result = ""
+
+for(let i = b; i >= a; i--) {
+    result += i + " "
+}
+console.log(result)
