928e32ead32991433294d9a39cae7e1564780e18
marchbom <153171193+marchbom@users.noreply.github.com>
2025-09-01 12:14:19 +0900

    Title: 자연수면 출력, Time: 190ms, Memory: 15MB, Status: Wrong Answer - Codetree

---
+++ 250901/자연수면 출력/output-only-natural-number.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().trim().split(" ").map(Number);
+
+let [a, b] = input;
+let result = ""
+
+if(a > 0) {
+    for(let i = 1; i <= b; i++) {
+        result += a
+    }
+}
+if(a < 0)  {
+    result = 0
+}
+console.log(result)
