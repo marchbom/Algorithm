96a67facfa8d54f249800f9d51ac104cdd041d80
marchbom <153171193+marchbom@users.noreply.github.com>
2025-09-05 15:54:44 +0900

    Title: 20대의 평균 나이, Time: 63ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 250905/20대의 평균 나이/average-age-of-20.js
+++ 250905/20대의 평균 나이/average-age-of-20.js
@@ -5,11 +5,11 @@ let result = 0
 let avg = 0;
 let count = 0
 for(let i = 0; i < input.length; i++) {
-    result += input[i]
-    count++;
     if(input[i] > 29) {
         break;
     }
+    result += input[i]
+    count++;
     avg = (result / count).toFixed(2)
 }
 
