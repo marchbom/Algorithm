2592a6162b4742257492c2e548974320f46757db
marchbom <153171193+marchbom@users.noreply.github.com>
2025-09-08 04:39:31 +0900

    Title: 공약수의 유무, Time: 66ms, Memory: 10MB, Status: Passed - Codetree

--- 250908/공약수의 유무/presence-or-absence-of-a-common-divisor.js
+++ 250908/공약수의 유무/presence-or-absence-of-a-common-divisor.js
@@ -2,14 +2,14 @@ const fs = require('fs');
 const input = fs.readFileSync(0).toString().trim().split(" ").map(Number);
 
 let [a, b] = input;
+let result = false
 
 
 for(let i = a; i <= b; i++) {
     if(1920 % i ===0 && 2880 % i === 0) {
-        console.log(1)
+        result = true;
         break;
-    } else {
-        console.log(0)
-        break
-    }
+    } 
 }
+
+console.log(result ? 1 : 0)
