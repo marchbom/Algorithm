44fbb5f737e019b59e4cf037b27ea449bfedcfc6
marchbom <153171193+marchbom@users.noreply.github.com>
2025-09-11 14:51:20 +0900

    Title: 정사각형 출력, Time: 66ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250911/정사각형 출력/print-square.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const n = Number(fs.readFileSync(0).toString().trim());
+
+let count = 1;
+
+for(let i = 1; i <= n; i++) {
+    let str = ""
+    for(let j = 1; j <= n; j++) {
+        str += count + " "
+        count++;
+    }
+    console.log(str)
+}
