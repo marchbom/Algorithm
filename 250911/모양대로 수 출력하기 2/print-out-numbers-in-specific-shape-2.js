6afb665eeb2b1bf733c9369884dedb535b19f735
marchbom <153171193+marchbom@users.noreply.github.com>
2025-09-11 15:38:03 +0900

    Title: 모양대로 수 출력하기 2, Time: 65ms, Memory: 10MB, Status: Passed - Codetree

--- 250911/모양대로 수 출력하기 2/print-out-numbers-in-specific-shape-2.js
+++ 250911/모양대로 수 출력하기 2/print-out-numbers-in-specific-shape-2.js
@@ -1,13 +1,14 @@
 const fs = require('fs');
 const n = Number(fs.readFileSync(0).toString().trim());
 
-let num = [2,4,6,8];
+let num = 2;
 
-
-for(let i = 0; i < n; i++) {
-    let str = ""
-    for(let j = 0; j < n; j++) {
-        str += num[(i + j) % num.length] + " "
-    }
-    console.log(str)
+for (let i = 0; i < n; i++) {
+  let str = "";
+  for (let j = 0; j < n; j++) {
+    str += num + " "
+    num += 2
+    if(num >= 10) num = 2
+  }
+  console.log(str.trim());
 }
