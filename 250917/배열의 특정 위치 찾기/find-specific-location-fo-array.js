06406328196f3e5960ac8b6229184599b7516ef1
marchbom <153171193+marchbom@users.noreply.github.com>
2025-09-17 15:22:54 +0900

    Title: 배열의 특정 위치 찾기, Time: 62ms, Memory: 10MB, Status: Passed - Codetree

--- 250917/배열의 특정 위치 찾기/find-specific-location-fo-array.js
+++ 250917/배열의 특정 위치 찾기/find-specific-location-fo-array.js
@@ -6,7 +6,7 @@ let sum2 = 0
 let avg = 0
 
 for(let i = 0; i < input.length; i++) {
-    if(input[i] % 2 === 0) {
+    if(i % 2 !== 0) {
         sum += input[i]
     }
     avg = ((input[2] + input[5] + input[8]) / 3).toFixed(1)
