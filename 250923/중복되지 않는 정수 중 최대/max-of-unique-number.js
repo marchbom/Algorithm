ab2c23a512e4ebbc4600d19ea75bf8e5bfd87a9b
marchbom <153171193+marchbom@users.noreply.github.com>
2025-09-23 12:29:17 +0900

    Title: 중복되지 않는 정수 중 최대, Time: 65ms, Memory: 10MB, Status: Passed - Codetree

--- 250923/중복되지 않는 정수 중 최대/max-of-unique-number.js
+++ 250923/중복되지 않는 정수 중 최대/max-of-unique-number.js
@@ -5,6 +5,6 @@ let n = input[0]
 let numbers = input[1].split(" ").map(Number)
 let unique = numbers.filter(num => numbers.indexOf(num) === numbers.lastIndexOf(num));
 
-let result = Math.max(unique) === 0 ? -1 : Math.max(unique)
+let result = unique.length === 0 ? -1 : Math.max(...unique);
 
 console.log(result)
