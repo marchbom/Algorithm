495dac9634200ff0c4331fd065f042e433ae6641
marchbom <153171193+marchbom@users.noreply.github.com>
2025-10-05 14:42:59 +0900

    [level 1] Title: 햄버거 만들기, Time: 215.83 ms, Memory: 85.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.js
@@ -0,0 +1,12 @@
+function solution(ingredient) {
+    let stack = []
+    let count = 0;
+    for(let i = 0; i < ingredient.length; i++) {
+        stack.push(ingredient[i]);
+        if(stack.slice(-4).join("") === "1231") {
+            stack.splice(-4);
+            count++;
+        }
+    }
+    return count;
+}
