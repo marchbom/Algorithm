9924b339be1c207fbe10c499937d6a5890a0614a
marchbom <153171193+marchbom@users.noreply.github.com>
2025-10-06 18:11:44 +0900

    Title: 더 긴 문자열, Time: 71ms, Memory: 10MB, Status: Passed - Codetree

--- 251006/더 긴 문자열/longer-string.js
+++ 251006/더 긴 문자열/longer-string.js
@@ -3,9 +3,9 @@ const str = fs.readFileSync(0).toString().trim().split(" ");
 
 
 if(str[0].length > str[1].length) {
-    console.log(`Coding ${str[0].length}`)
+    console.log(`${str[0]} ${str[0].length}`)
 } else if (str[0].length < str[1].length) {
-    console.log(`Coding ${str[1].length}`)
+    console.log(`${str[1]} ${str[1].length}`)
 } else {
     console.log("same")
 }
