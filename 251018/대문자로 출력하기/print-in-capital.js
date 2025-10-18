d467b25355eaecbc806a22055c4e5be27728ffd9
Bomin Kim <153171193+marchbom@users.noreply.github.com>
2025-10-18 23:01:05 +0900

    Title: 대문자로 출력하기, Time: 61ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 251018/대문자로 출력하기/print-in-capital.js
+++ 251018/대문자로 출력하기/print-in-capital.js
@@ -1,10 +1,8 @@
 const fs = require('fs')
-const input = fs.readFileSync(0).toString().trim().split(".").join("")
+const input = fs.readFileSync(0).toString().trim()
 
-let result = []
 for(const s of input) {
     if(!isNaN(Number(s))) continue;
-    else result.push(s);
 }
 
-console.log(result.join("").toUpperCase())
+console.log(input.toUpperCase())
