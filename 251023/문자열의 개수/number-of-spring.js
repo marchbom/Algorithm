5074e9e7574d15d367f339b401599842e36cd0f3
Bomin Kim <153171193+marchbom@users.noreply.github.com>
2025-10-23 20:19:30 +0900

    Title: 문자열의 개수, Time: 64ms, Memory: 10MB, Status: Passed - Codetree

--- 251023/문자열의 개수/number-of-spring.js
+++ 251023/문자열의 개수/number-of-spring.js
@@ -2,12 +2,15 @@ const fs = require('fs')
 const input = fs.readFileSync(0).toString().trim().split("\n");
 
 
-console.log(input.length - 1);
-for(let i = 0; i <= input.length; i++) {
+let count = 0;
+let result = []
+for(let i = 0; i < input.length; i++) {
+    if(input[i] === "0") break;
+    count++;
     if(i % 2 === 0) {
-        console.log(input[i])
+        result.push(input[i])
     }
-    if(input[i] === "0") {
-        break;
-    } 
 }
+
+console.log(count)
+console.log(result.join("\n"))
