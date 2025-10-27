d4bef16ef62c86cd34c8d01f108c0c633464932b
Bomin Kim <153171193+marchbom@users.noreply.github.com>
2025-10-27 11:52:59 +0900

    [level 0] Title: 조건 문자열, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181934. 조건 문자열/조건 문자열.js
@@ -0,0 +1,8 @@
+function solution(ineq, eq, n, m) {
+
+    if(ineq === "<" && eq === "=") return n <= m ? 1 : 0
+    if(ineq === ">" && eq === "=") return n >= m ? 1 : 0
+    if(ineq === ">" && eq === "!") return n > m ? 1 : 0
+    if(ineq === "<" && eq === "!") return n < m ? 1 : 0
+
+}
