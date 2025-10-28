c70d1102c43cd8092d8cf6f8639db7c3d4189f5e
Bomin Kim <153171193+marchbom@users.noreply.github.com>
2025-10-28 11:25:30 +0900

    Title: 두 수의 거듭제곱, Time: 69ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251028/두 수의 거듭제곱/two-numbers-of-squares.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split(" ");
+const [a, b] = input.map(Number);
+// Please Write your code here. 
+
+function solution(a, b) {
+    return Math.pow(a, b)
+}
+
+console.log(solution(a, b))
