ceb13125e13e318503dbf47b134116ba9f34b235
Bomin Kim <153171193+marchbom@users.noreply.github.com>
2025-11-07 12:18:11 +0900

    Title: 재귀함수를 이용한 별 출력, Time: 65ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251107/재귀함수를 이용한 별 출력/star-output-with-recursive-function.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+let n = Number(input[0]);
+// Please Write your code here.
+
+function printStr(n) {
+    if(n === 0) return;
+    printStr(n - 1)
+    console.log("*".repeat(n))
+}
+
+printStr(n)
