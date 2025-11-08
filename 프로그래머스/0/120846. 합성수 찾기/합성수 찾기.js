6593d44ad2cf61701bf0fb1096f0525f84e6ae2a
Bomin Kim <153171193+marchbom@users.noreply.github.com>
2025-11-08 17:16:17 +0900

    [level 0] Title: 합성수 찾기, Time: 0.20 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.js
@@ -0,0 +1,11 @@
+function solution(n) {
+    var result = 0;
+    for(let i = 1; i <= n; i++) {
+        let count = 0;
+        for(let j = 1; j <= i; j++) {
+            if(i % j === 0) count++
+        }
+        if(count >= 3) result++;
+    }
+    return result;
+}
