a4b8d553e645d8aaf469e287aad823d0461311aa
Bomin Kim <153171193+marchbom@users.noreply.github.com>
2025-11-09 15:20:32 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.24 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -1,7 +1,31 @@
 function solution(s) {
-    let word = ["zero", "one","two","three","four", "five","six","seven","eight", "nine"];
-    word.forEach((str, idx) => {
-       s = s.replaceAll(str, idx)
-    })
-    return Number(s)
+    let wordMap = {
+        "zero" : 0,
+        "one" : 1,
+        "two" : 2,
+        "three" : 3,
+        "four" : 4,
+        "five" : 5,
+        "six" : 6,
+        "seven" : 7,
+        "eight" : 8,
+        "nine" : 9
+    };
+    
+    let result = []
+    let temp = ""
+    
+    // 정규표현식으로 숫자라면 push하고 아니라면 문자열을 하나씩 더해서 wordMap에 맞는 숫자값을 push하도록
+    for(let ch of s) {
+        if(/\d/.test(ch)) {
+            result.push(ch)
+        } else {
+            temp += ch;
+            if(wordMap[temp] !== undefined) {
+                result.push(wordMap[temp]);
+                temp = "";
+            }
+        }
+    }
+    return Number(result.join(""));
 }
