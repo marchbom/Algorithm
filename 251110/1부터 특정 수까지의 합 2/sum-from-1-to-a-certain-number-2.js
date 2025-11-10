50c1bc248b2f17580073ddfd877725cd816beb64
Bomin Kim <153171193+marchbom@users.noreply.github.com>
2025-11-10 12:24:54 +0900

    Title: 1부터 특정 수까지의 합 2, Time: 67ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251110/1부터 특정 수까지의 합 2/sum-from-1-to-a-certain-number-2.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const n = Number(input[0]);
+
+// Please Write your code here.
+
+function fac(n) {
+    if(n === 0) return 0;
+    
+    return fac(n-1) + n
+}
+
+console.log(fac(n))
