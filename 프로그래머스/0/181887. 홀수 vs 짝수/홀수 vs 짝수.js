c25f31ba66c193ca80f1d205b310070b08d5cda5
Bomin Kim <153171193+marchbom@users.noreply.github.com>
2025-11-25 14:39:12 +0900

    [level 0] Title: 홀수 vs 짝수, Time: 0.19 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.js
@@ -0,0 +1,12 @@
+function solution(num_list) {
+    let evenSum = 0;
+    let oddSum = 0;
+    for(let i = 0; i < num_list.length; i++) {
+        if((i + 1) % 2 !== 0) {
+            oddSum += num_list[i]
+        } else {
+            evenSum += num_list[i]
+        }
+    }
+    return Math.max(evenSum, oddSum);
+}
