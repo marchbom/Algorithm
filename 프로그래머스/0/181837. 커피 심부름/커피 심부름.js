6ed43deae60fea3993f5ebcf5c58105f94ef461b
Bomin Kim <153171193+marchbom@users.noreply.github.com>
2025-12-17 14:30:38 +0900

    [level 0] Title: 커피 심부름, Time: 0.33 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181837. 커피 심부름/커피 심부름.js
+++ 프로그래머스/0/181837. 커피 심부름/커피 심부름.js
@@ -1,11 +1,7 @@
 function solution(order) {
     let result = 0;
-    let americanoList = ["iceamericano", "americanoice", "hotamericano", "americanohot","americano"]
-    let latteList = ["icecafelatte", "cafelatteice", "hotcafelatte", "cafelattehot", "cafelatte"]
-    for(let i = 0; i < order.length; i++) {
-        if(americanoList.includes(order[i])) {
-            result += 4500;
-        } else if (latteList.includes(order[i])) {
+    for(const coffee of order) {
+        if(coffee.includes("cafelatte")) {
             result += 5000
         } else {
             result += 4500
